10b8c06eb59cf5c02330a3d32417b3768e0ef3a0
zmd58 <zdang@wgu.edu>
2024-07-17 18:24:24 -0400

    renamed repository classes

--- src/main/java/com/framework/CRUD/sampleitem/SampleItem.java
+++ src/main/java/com/framework/CRUD/sampleitem/SampleItem.java
@@ -1,11 +1,16 @@
 package com.framework.CRUD.sampleitem;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public record SampleItem(
         int id,
+        @NotNull
         String name,
         String description,
+        @Positive
         BigDecimal price,
         SampleType sampleType
 ) {
--- src/main/java/com/framework/CRUD/sampleitem/SampleItemController.java
+++ src/main/java/com/framework/CRUD/sampleitem/SampleItemController.java
@@ -1,8 +1,8 @@
 package com.framework.CRUD.sampleitem;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +11,7 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class SampleItemController {
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     private final SampleItemIRepository sampleItemIRepository;
 
@@ -22,60 +23,88 @@ public class SampleItemController {
     public SampleItemController(SampleItemRepository sampleItemRepository) {
         this.sampleItemRepository = sampleItemRepository;
 >>>>>>> 138388e (added .http file)
+=======
+    private final SampleItemIRepository sampleItemIRepository;
+
+    public SampleItemController(SampleItemIRepository sampleItemIRepository) {
+        this.sampleItemIRepository = sampleItemIRepository;
+>>>>>>> 81bd8e1 (renamed repository classes)
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping()
     List<SampleItem> getAllItems() {
 <<<<<<< HEAD
+<<<<<<< HEAD
         return sampleItemIRepository.findAll();
 =======
         return sampleItemRepository.findAll();
 >>>>>>> 138388e (added .http file)
+=======
+        return sampleItemIRepository.findAll();
+>>>>>>> 81bd8e1 (renamed repository classes)
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     SampleItem getItemById(@PathVariable int id) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         Optional<SampleItem> item = sampleItemIRepository.findById(id);
 =======
         Optional<SampleItem> item = sampleItemRepository.findById(id);
 >>>>>>> 138388e (added .http file)
+=======
+        Optional<SampleItem> item = sampleItemIRepository.findById(id);
+>>>>>>> 81bd8e1 (renamed repository classes)
         if (item.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new SampleItemNotFoundException();
         }
         return item.get();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
+<<<<<<< HEAD
     void createItem(@RequestBody SampleItem sampleItem) {
 <<<<<<< HEAD
         sampleItemIRepository.create(sampleItem);
 =======
         sampleItemRepository.create(sampleItem);
 >>>>>>> 138388e (added .http file)
+=======
+    void createItem(@Valid @RequestBody SampleItem sampleItem) {
+        sampleItemIRepository.create(sampleItem);
+>>>>>>> 81bd8e1 (renamed repository classes)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
+<<<<<<< HEAD
     void updateItem(@RequestBody SampleItem sampleItem, @PathVariable int id) {
 <<<<<<< HEAD
         sampleItemIRepository.update(sampleItem, id);
 =======
         sampleItemRepository.update(sampleItem, id);
 >>>>>>> 138388e (added .http file)
+=======
+    void updateItem(@Valid @RequestBody SampleItem sampleItem, @PathVariable int id) {
+        sampleItemIRepository.update(sampleItem, id);
+>>>>>>> 81bd8e1 (renamed repository classes)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     void deleteById(@PathVariable int id) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         sampleItemIRepository.delete(id);
 =======
         sampleItemRepository.delete(id);
 >>>>>>> 138388e (added .http file)
+=======
+        sampleItemIRepository.delete(id);
+>>>>>>> 81bd8e1 (renamed repository classes)
     }
 
 }
